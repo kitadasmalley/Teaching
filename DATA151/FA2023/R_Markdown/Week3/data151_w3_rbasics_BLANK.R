34c02bc09ed557d10d78241bdd1a5d09a0065914
kitadasmalley <72528399+kitadasmalley@users.noreply.github.com>
2023-10-05 17:40:55 -0700

    Add files via upload

---
+++ DATA151/FA2023/R_Markdown/Week3/data151_w3_rbasics_BLANK.R
@@ -0,0 +1,88 @@
+## NOTES FOR CLASS 3A and 3B
+## YOUR NAME HERE
+
+## STEP 1: Install Packages
+## YOUR CODE HERE ###
+
+## STEP 2: Calling the library
+## YOUR CODE HERE ###
+
+## STEP 3: Data sets in the Environment
+## YOUR CODE HERE ###
+
+## STEP 4: Learning about the data
+help("OrchardSprays")
+## YOUR CODE HERE ###
+
+### What does the experiment look like?
+## YOUR CODE HERE ###
+
+
+## STEP 5: Looking at the data
+## head: first six rows
+## YOUR CODE HERE ###
+
+## tail: last six rows
+## YOUR CODE HERE ###
+
+## View: creates a new tab to see the data
+## YOUR CODE HERE ###
+
+## STEP 6: Data structure
+## YOUR CODE HERE ###
+
+
+## STEP 7: Variable Assignment and $ Operator
+## YOUR CODE HERE ###
+
+## what kind of class is this?
+## YOUR CODE HERE ###
+
+## there are 4 types of classes
+# 1) factors
+## YOUR CODE HERE ###
+
+# 2) character strings
+## YOUR CODE HERE ###
+
+# 3) logic
+## YOUR CODE HERE ###
+
+# 4) built in numbers
+## YOUR CODE HERE ###
+
+# 5) integers (whole numbers)
+## YOUR CODE HERE ###
+
+## STEP 8: Graphics in base R
+## YOUR CODE HERE ###
+
+## BONUS: Reorder factors
+### Is this the order we want?
+## YOUR CODE HERE ###
+
+## Plot again
+## YOUR CODE HERE ###
+
+
+## STEP 9: Vectors
+### vectors are one dimensional arrays
+## YOUR CODE HERE ###
+
+
+## STEP 10: Common functions
+## how much solution was consumed in the experiment?
+## YOUR CODE HERE ###
+
+## what is the average amount of solution consumed?
+## YOUR CODE HERE ###
+
+## STEP 11: Using variables in operations
+## Use the sum operator
+## YOUR CODE HERE ###
+
+# verify
+## YOUR CODE HERE ###
+
+
+
